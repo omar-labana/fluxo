bf8c151ea7baba8d189510a0220e881895811728
omar-labana <omar.h.labana@gmail.com>
2024-02-17 22:36:40 +0200

    asd

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
   <div>Hello, World!</div>
 </template>
+
+<script setup>
+onMounted(() => {
+  // document.documentElement
+  document.documentElement.setAttribute("data-bs-theme", "dark");
+});
+</script>
